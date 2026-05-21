d9aa202bd73beee6e31e93bb598dea862b3a6922
soulbyte07 <soul3147@proton.me>
2026-05-21 11:16:54 +0530

    main.tf: + provider

--- terraform/main.tf
+++ terraform/main.tf
@@ -8,3 +8,28 @@ terraform {
     }
   }
 }
+
+provider "aws" {
+  region                      = var.region
+  access_key                  = "test"
+  secret_key                  = "test"
+  skip_credentials_validation = true
+  skip_metadata_api_check     = true
+  skip_requesting_account_id  = true
+
+  endpoints {
+    apigateway     = "http://localhost:4566"
+    cloudformation = "http://localhost:4566"
+    cloudwatch     = "http://localhost:4566"
+    dynamodb       = "http://localhost:4566"
+    ec2            = "http://localhost:4566"
+    iam            = "http://localhost:4566"
+    kms            = "http://localhost:4566"
+    lambda         = "http://localhost:4566"
+    logs           = "http://localhost:4566"
+    s3             = "http://localhost:4566"
+    sns            = "http://localhost:4566"
+    sqs            = "http://localhost:4566"
+    sts            = "http://localhost:4566"
+  }
+}
