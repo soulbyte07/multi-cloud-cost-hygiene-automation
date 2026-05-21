028c1d6760e5c263aea17a1979672c7c41df3e39
soulbyte07 <soul3147@proton.me>
2026-05-21 12:02:21 +0530

    output: + logbucket

--- terraform/output.tf
+++ terraform/output.tf
@@ -8,3 +8,7 @@ output "public_subnet_ids" {
   value       = module.network.public_subnet_ids
 }
 
+output "logs_bucket_name" {
+  description = "S3 bucket name for logs."
+  value       = aws_s3_bucket.logsBucket.id
+}
