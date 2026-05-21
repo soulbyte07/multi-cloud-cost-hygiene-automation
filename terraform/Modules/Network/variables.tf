fb9d05558cb4f76296463ce2b1a2a88e852680da
soulbyte07 <soul3147@proton.me>
2026-05-21 11:41:45 +0530

    net/var.tf: + cidr, subnet

---
+++ terraform/Modules/Network/variables.tf
@@ -0,0 +1,11 @@
+variables "vpc_cidr_block" {
+  description = "CIDR block for the VPC"
+  type        = string
+}
+
+variable "public_subnet_cidrs" {
+  description = "List of CIDR blocks for public subnets"
+  type        = list(string)
+}
+
+
