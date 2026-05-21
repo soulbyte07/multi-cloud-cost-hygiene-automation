276b2b6d3014b83dddbba34b14f9ac934a1537e5
soulbyte07 <soul3147@proton.me>
2026-05-21 11:31:50 +0530

    net: main: + vpc, igw, public subnet

---
+++ terraform/Modules/Network/main.tf
@@ -0,0 +1,27 @@
+resource "aws_vpc" "mainVpc" {
+  cidr_block = var.vpc_cidr
+  enable_dns_support   = true
+  enable_dns_hostnames = true
+  tags = {
+    Name = "nimbuskart-vpc"
+  }
+}
+
+
+resource "aws_internet_gateway" "mainIgw" {
+  vpc_id = aws_vpc.mainVpc.id
+  tags = {
+    Name = "nimbuskart-igw"
+  }
+}
+
+resource "aws_subnet" "publicSubnet" {
+  vpc_id     = aws_vpc.mainVpc.id
+  cidr_block = var.subnet_cidr
+  availability_zone = var.availability_zone
+  count = length(var.public_subnet_cidrs)
+  map_public_ip_on_launch = true
+  tags = {
+    Name = "nimbuskart-public-subnet"
+  }
+}
