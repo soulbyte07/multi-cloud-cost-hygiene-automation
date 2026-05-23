7e7b1fe8618b037f08b322ef7f747c9a1d91cc09
soulbyte07 <soul3147@proton.me>
2026-05-23 11:16:07 +0530

    janitor.py: + scan_aws_resources() with scan for Ec2 instanaces, EBL, EIP

---
+++ nimbuskart-janitor/janitor.py
@@ -0,0 +1,72 @@
+import boto3 
+
+
+# Scan the AWS 
+def scan_aws_resources():
+    # Create a session using your AWS credentials
+    session = boto3.Session(
+            # TODO: Replace with Sops or AWS Secrets Manager
+        aws_access_key_id="YOUR_ACCESS",
+    )
+    # Create a client for the AWS service to scan for Resources
+    ec2_client = session.client('ec2')
+    # Scan for EC2 instances
+    response = ec2_client.describe_instances()
+    instances = []
+    for reservation in response['Reservations']:
+        for instance in reservation['Instances']:
+            instances.append({
+                'InstanceId': instance['InstanceId'],
+                'State': instance['State']['Name'],
+                'LaunchTime': instance['LaunchTime']
+            })
+
+    # Scan for EBS volumes
+    ebs_client = session.client('ec2')
+    response = ebs_client.describe_volumes()
+    volumes = []
+    for volume in response['Volumes']:
+        volumes.append({
+            'VolumeId': volume['VolumeId'],
+            'State': volume['State'],
+            'CreateTime': volume['CreateTime']
+        })
+
+
+    # Scan for EIP
+    eip_client = session.client('ec2')
+    response = eip_client.describe_addresses()
+    eips = []
+    for address in response['Addresses']:
+        eips.append({
+            'PublicIp': address['PublicIp'],
+            'AllocationId': address.get('AllocationId', 'N/A'),
+            'AssociationId': address.get('AssociationId', 'N/A') })
+
+    return {
+        'instances': instances,
+        'volumes': volumes,
+        'eips': eips
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
